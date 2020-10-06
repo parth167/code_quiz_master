21a533c84ef970a17133e47950a48d757faeabe3
patel <parthpatel167@gmail.com>
2020-10-06 17:39:37 -0500

    quiz question apdated

---
+++ question.js
@@ -0,0 +1,30 @@
+// function loadQuestions() {
+    const questions = [
+        {
+            title: "Question 1: Commonly used data types DO NOT include:",
+            choices: ["strings", "booleans", "alerts", "numbers"],
+            answer: "alerts"
+        },
+        {
+            title: "Question 2: The instructions for a function are enclosed within ____.",
+            choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+            answer: "curly brackets"
+        },
+        {
+            title: "Question 3: What does the DOM stand for?",
+            choices: ["Document Object Model","Document Outline Model","Doctype Object Model","Device Object Model"],
+            answer: "method"
+        },
+        {
+            title: "Question 4: The logical operator that represents 'or' is ____.",
+            choices: ["||", "OR", "&&", "==="],
+            answer: "||"
+        },
+        {
+            title: "Question 5: How would you write an IF statement in JavaScript?",
+            choices: ["if (i == 5)", "if i == 5 then", "if i = 5 then", "if i = 5"],
+            answer: "if (i == 5)"
+        }
+    ];    
+// };
+// loadQuestions();
