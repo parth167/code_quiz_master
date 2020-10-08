acdc015cc5121980ee0244fbcf6584e73ebbf638
patel <parthpatel167@gmail.com>
2020-10-08 13:20:39 -0500

    question array update

--- question.js
+++ question.js
@@ -1,5 +1,5 @@
 // function loadQuestions() {
-    const questions = [
+    var  questions = [
         {
             title: "Question 1: Commonly used data types DO NOT include:",
             choices: ["strings", "booleans", "alerts", "numbers"],
@@ -13,16 +13,16 @@
         {
             title: "Question 3: What does the DOM stand for?",
             choices: ["Document Object Model","Document Outline Model","Doctype Object Model","Device Object Model"],
-            answer: "method"
+            answer: "Document Object Model"
         },
         {
             title: "Question 4: The logical operator that represents 'or' is ____.",
-            choices: ["||", "OR", "&&", "==="],
+            choices: ["OR", "||", "&&", "==="],
             answer: "||"
         },
         {
             title: "Question 5: How would you write an IF statement in JavaScript?",
-            choices: ["if (i == 5)", "if i == 5 then", "if i = 5 then", "if i = 5"],
+            choices: [ "if i == 5 then", "if i = 5 then", "if (i == 5)", "if i = 5"],
             answer: "if (i == 5)"
         }
     ];    
